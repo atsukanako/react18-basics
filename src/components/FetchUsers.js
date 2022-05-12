6a75fc7f74850498bfdfb5023ddeef1d818793c3
Kanako Kokou <atsukanako@gmail.com>
2022-05-12 14:49:56 +0900

    First Commit

---
+++ src/components/FetchUsers.js
@@ -0,0 +1,18 @@
+import { useQueryUsers } from '../hooks/useQueryUsers'
+
+export const FetchUsers = () => {
+  const { data } = useQueryUsers()
+  // if (status === 'loading) return <p>Loaing...</p>
+  // if (status === 'error') return <p>Error</p>
+
+  return (
+    <div className="my-3 text-center">
+      <p className="my-3 font-bold">User List</p>
+      {data?.map((user) => (
+        <p className="my-3 text-sm" key={user.id}>
+          {user.username}
+        </p>
+      ))}
+    </div>
+  )
+}
